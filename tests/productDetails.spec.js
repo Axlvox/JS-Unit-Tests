926ed6dda0c81cc706961b972946bbc5cfec71b4
Axlvox <leandro.jlds@gmail.com>
2023-05-08 03:50:16 -0300

    Requisito 6 completo

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -30,14 +30,33 @@ const productDetails = require('../src/productDetails');
 */
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
-  it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    fail('Teste vazio!');
-    // ESCREVA SEUS TESTES ABAIXO:
-    // Teste se productDetails é uma função.
-    // Teste se o retorno da função é um array.
-    // Teste se o array retornado pela função contém dois itens dentro.
-    // Teste se os dois itens dentro do array retornado pela função são objetos.
-    // Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.
-    // Teste se os dois productIds terminam com 123.
+    it('Teste se productDetails é uma função.', () => {
+      expect(typeof productDetails).toBe('function');
+       // Teste se productDetails é uma função.
+    });
+    it('Teste se o retorno da função é um array.', () => {
+      let seraArray = productDetails();
+      expect(Array.isArray(seraArray)).toBe(true);
+     // Teste se o retorno da função é um array.
+    });
+    it('Teste se o array retornado pela função contém dois itens dentro.', () => {
+      expect(productDetails).toHaveLength(2);
+     // Teste se o array retornado pela função contém dois itens dentro.
+    });
+    it('Teste se os dois itens dentro do array retornado pela função são objetos.', () => {
+      expect(productDetails).toBeInstanceOf(Object);
+     // Teste se os dois itens dentro do array retornado pela função são objetos.
+    });
+    it('Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.', () => {
+      let [produto1, produto2] = productDetails('produto1', 'produto2');
+      expect(produto1).not.toEqual(produto2);
+     // Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.
+    });
+    it('Teste se os dois productIds terminam com 123.', () => {
+      const products = productDetails("Product1", "Product2");
+      products.forEach(product => {
+        expect(product.details.productId.endsWith("123")).toBe(true);
+       // Teste se os dois productIds terminam com 123.
+    });
   });
 });
