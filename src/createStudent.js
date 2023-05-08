b23f714a3a15d5561a24dc4da41b804acb0c9c50
Axlvox <leandro.jlds@gmail.com>
2023-05-08 02:50:05 -0300

    Requisito 5 completo

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,9 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (named) => ({
+  name: named,
+  feedback: () => 'Eita pessoa boa!',
+});
 
 module.exports = createStudent;
